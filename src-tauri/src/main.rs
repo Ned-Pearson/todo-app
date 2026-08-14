e008b727652af1f3931d7087d18f2a9e10b189a8
Edward Pearson <s3844470@student.rmit.edu.au>
2026-08-14 12:43:39 +1000

    Add tauri-plugin-updater dependency and permissions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -231,6 +231,7 @@ fn main() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
